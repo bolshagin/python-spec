d3b9149ff68d57f69716de39a10bdf972e1eda20
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2020-04-11 14:20:52 +0500

    Неделя №2. ДЗ №2

---
+++ 1-dive-in-python/to_json.py
@@ -0,0 +1,9 @@
+import json
+from functools import wraps
+
+
+def to_json(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        return json.dumps(func(*args, **kwargs))
+    return wrapped
