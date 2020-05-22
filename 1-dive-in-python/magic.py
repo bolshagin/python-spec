d56c6caf08a5787c5dd09a679e9682e79943e866
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2020-05-22 19:02:20 +0500

    Неделя №4. ДЗ №1 (решено)

--- 1-dive-in-python/magic.py
+++ 1-dive-in-python/magic.py
@@ -4,28 +4,51 @@ from tempfile import gettempdir
 
 class File:
 
-    def __init__(self, path_to_file):
-        self.path_to_file = path_to_file
+    def __init__(self, path):
+        self.path_to_file = path
+        if not os.path.exists(path):
+            with open(path, 'w'):
+                pass
 
     def read(self):
-        pass
+        with open(self.path_to_file, 'r') as f:
+            res = f.read()
+        return res
 
-    def write(self, text):
-        pass
+    def write(self, text, mode='w'):
+        with open(self.path_to_file, mode) as f:
+            f.write(text)
 
     def __add__(self, other):
-        pass
+        new_path = os.path.join(gettempdir(), 'temp.txt')
+        new_file = File(new_path)
+
+        new_file.write(self.read(), 'w')
+        new_file.write(other.read(), 'a')
+        return new_file
 
     def __iter__(self):
-        pass
+        self.current_position = 0
+        return self
 
     def __next__(self):
-        pass
+        with open(self.path_to_file, 'r') as f:
+            f.seek(self.current_position)
+            line = f.readline()
+            if not line:
+                self.current_position = 0
+                raise StopIteration('End of file')
+
+            self.current_position = f.tell()
+
+        return line
 
     def __repr__(self):
-        return self.path_to_file
+        return os.path.abspath(self.path_to_file)
 
 
 if __name__ == '__main__':
-    file_obj = File('path_to_file')
-    print(file_obj)
+    path_to_file = 'some_filename'
+    file_obj = File(path_to_file)
+    for line in file_obj:
+        print(line)
