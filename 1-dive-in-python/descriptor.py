3fdc97f228689f9faa7c4082fcd2716d3a2e6880
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2020-05-22 19:05:03 +0500

    Неделя №4. ДЗ №2 (каркас)

---
+++ 1-dive-in-python/descriptor.py
@@ -0,0 +1,13 @@
+class Value:
+    pass
+
+
+class Account:
+    amount = Value()
+
+    def __init__(self, commission):
+        self.commission = commission
+
+
+if __name__ == '__main__':
+    pass
