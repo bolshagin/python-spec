be573b1164ca2222948369c9041c894d1eb080bd
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2020-06-15 22:08:52 +0500

    Неделя №5. ДЗ №1. Добавил соединение в класс клиента. Добавил пример сервера для тестирования метода get.

--- 1-dive-in-python/hw5/client.py
+++ 1-dive-in-python/hw5/client.py
@@ -6,14 +6,27 @@ class ClientException(Exception):
 
 
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, address, port, timeout=None):
+        self.address = address
+        self.port = port
+        self.timeout = timeout
+
+        try:
+            self.connection = socket.create_connection((address, port), timeout=timeout)
+        except socket.error as err:
+            raise ClientException('Невозможно установить соединение', err)
 
     def get(self):
         pass
 
     def put(self):
         pass
+    
+    def close(self):
+        try:
+            self.connection.close()
+        except socket.error as err:
+            raise ClientException('Невозможно закрыть соединение', err)
 
 
 if __name__ == '__main__':
