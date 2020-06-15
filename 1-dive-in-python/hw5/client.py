00d8d3df32acdaef66b1e6e3ca7fa717cff8c7e8
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2020-06-15 21:31:27 +0500

    Неделя №5. ДЗ №1 (стуктура клиента)

---
+++ 1-dive-in-python/hw5/client.py
@@ -0,0 +1,20 @@
+import socket
+
+
+class ClientException(Exception):
+    pass
+
+
+class Client:
+    def __init__(self):
+        pass
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
